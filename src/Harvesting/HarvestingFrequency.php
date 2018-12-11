8e0ca51ea9752c7bd4f3d4d9a1b62787edb648ed
Willem <wrpterberg@gmail.com>
2018-12-11 16:58:11 +0100

    included support for distribution buildrules

--- src/Harvesting/HarvestingFrequency.php
+++ src/Harvesting/HarvestingFrequency.php
@@ -35,4 +35,37 @@ class HarvestingFrequency
     {
         return in_array($frequency, [self::DAILY, self::WEEKLY, self::MONTHLY]);
     }
+
+    /**
+     * Returns all the eligible harvesting frequencies data source modules can have that are
+     * eligible for harvesting based on the requested harvesting frequency.
+     *
+     * Basically, any harvesting frequency which has a higher frequency than the given frequency and
+     * the given frequency will be eligible. This means that a when a weekly frequency is given, a
+     * daily frequency is also allowed. When monthly is provided, weekly and daily are also
+     * eligible.
+     *
+     * @param string $frequency The input frequency to check with
+     *
+     * @return string[] The eligible harvesting frequencies
+     */
+    public static function determineEligibleHarvestingFrequencies(string $frequency): array
+    {
+        if (self::MONTHLY === $frequency) {
+            return [
+                self::MONTHLY,
+                self::WEEKLY,
+                self::DAILY,
+            ];
+        }
+
+        if (self::WEEKLY === $frequency) {
+            return [
+                self::WEEKLY,
+                self::DAILY,
+            ];
+        }
+
+        return [self::DAILY];
+    }
 }
